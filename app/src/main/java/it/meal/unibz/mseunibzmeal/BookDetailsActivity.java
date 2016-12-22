3d2ade254ecdbd160a88f1cd82a4d47efb1fb408
astan <AlexandruCristian.Stan@stud-inf.unibz.it>
2016-12-22 17:56:43 +0100

    8

--- app/src/main/java/it/meal/unibz/mseunibzmeal/BookDetailsActivity.java
+++ app/src/main/java/it/meal/unibz/mseunibzmeal/BookDetailsActivity.java
@@ -7,11 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.Toast;
 
 public class BookDetailsActivity extends AppCompatActivity {
     private EditText BookName;
     private EditText AuthorName;
     private EditText Availability;
+    private TableLayout tableLayout;
 
     private int bookID;
 
@@ -19,10 +22,18 @@ public class BookDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_details);
+        tableLayout=(TableLayout)findViewById(R.id.tblBookList);
 
         this.BookName = (EditText)findViewById(R.id.book_name);
         this.AuthorName = (EditText)findViewById(R.id.author_name);
         this.Availability = (EditText)findViewById(R.id.availability);
+
+        try {
+
+        }catch (Exception ex) {
+            Toast.makeText(getApplicationContext(),ex.getMessage(),Toast.LENGTH_LONG).show();
+
+        }
     }
 
 }
